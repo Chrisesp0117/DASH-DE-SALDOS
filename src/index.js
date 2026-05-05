a0181acb3c631ba5de24479b0ea84f755bca8058
Chrisesp0117 <chrisesp0117@gmail.com>
2026-05-05 13:09:45 -0400

    fix: clean up src/index.js handler to avoid build parse error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 require('dotenv').config({ path: '.env' });
 
-const { getSheets } = require('./services/sheets');
 module.exports = (req, res) => {
   res.status(200).send('OK');
 };
-      process.exit(1);
-    });
-}
