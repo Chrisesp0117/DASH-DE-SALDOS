dee2097b05f65f58b501b8774f86c971b842710b
Chrisesp0117 <chrisesp0117@gmail.com>
2026-05-06 10:12:21 -0400

    fix: use São Paulo timezone for last update timestamp

--- src/run.js
+++ src/run.js
@@ -17,12 +17,14 @@ function isValidGoogleCustomerId(value) {
 
 function formatLastUpdatePTBR(date = new Date()) {
   const datePart = new Intl.DateTimeFormat('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
     day: '2-digit',
     month: '2-digit',
     year: 'numeric'
   }).format(date);
 
   const timePart = new Intl.DateTimeFormat('pt-BR', {
+    timeZone: 'America/Sao_Paulo',
     hour: '2-digit',
     minute: '2-digit'
   }).format(date);
